b2361428099cb4dc1ffad44353eb35f8f81ea88c
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-18 23:27:44 +0100

    Rename variable from player to paddle

--- player.py
+++ player.py
@@ -3,25 +3,25 @@ import turtle as t
 
 class Player:
     def __init__(self):
-        self.player = t.Turtle()
-        self.player.shape('square')
-        self.player.color('green')
-        self.player.shapesize(stretch_wid=1, stretch_len=5)
-        self.player.penup()
-        self.player.goto(0, -280)
-        self.player.speed(1)
+        self.paddle = t.Turtle()
+        self.paddle.shape('square')
+        self.paddle.color('green')
+        self.paddle.shapesize(stretch_wid=1, stretch_len=5)
+        self.paddle.penup()
+        self.paddle.goto(0, -280)
+        self.paddle.speed(1)
 
     def move_left(self):
-        x = self.player.xcor()
+        x = self.paddle.xcor()
         x -= 20
         if x < -347:
             x = -347
-        self.player.setx(x)
+        self.paddle.setx(x)
 
     def move_right(self):
-        x = self.player.xcor()
+        x = self.paddle.xcor()
         x += 20
         if x > 340:
             x = 340
-        self.player.setx(x)
+        self.paddle.setx(x)
 
