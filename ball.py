745d32b85f14b073ffb8ba01a514c09f73e9808c
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-18 16:16:29 +0100

    Write method for ball changing position

--- ball.py
+++ ball.py
@@ -8,3 +8,7 @@ class Ball:
         self.ball.color(color)
         self.ball.penup()
         self.ball.goto(0, 0)
+
+    def move(self):
+        self.ball.setx(self.ball.xcor() + 2)
+        self.ball.sety(self.ball.ycor() + 2)
