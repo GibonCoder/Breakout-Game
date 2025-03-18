046a54cbd6d08e4e9b7f0065aa77f52bf7ca825b
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-18 16:04:03 +0100

    Create method for running window

--- screen.py
+++ screen.py
@@ -7,3 +7,6 @@ class Screen:
         self.screen.title("Breakout Game")
         self.screen.bgcolor('black')
         self.screen.setup(width=800, height=600)
+
+    def run_screen(self):
+        self.screen.mainloop()
