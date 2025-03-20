3376826997875913a12be2f010c88c4f609ee86b
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-03-20 20:02:36 +0100

    Write method for defining size of brick

--- brick.py
+++ brick.py
@@ -1,7 +1,18 @@
 import turtle as t
+from random_color_generator import random_color_generator as random_color
 
 
 class Brick:
     def __init__(self):
         self.brick = t. Turtle()
         self.brick.shape('square')
+        self.brick.color(random_color())
+
+    def set_size(self, size):
+        match size:
+            case 'small':
+                self.brick.shapesize(stretch_len=1, stretch_wid=1)
+            case ' medium':
+                self.brick.shapesize(stretch_len=2, stretch_wid=1)
+            case 'large':
+                self.brick.shapesize(stretch_len=3, stretch_wid=1)
