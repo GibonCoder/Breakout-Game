9cd70bda2d96c72c3195e46eb08a2f2831f3c6b1
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-04-07 21:36:20 +0200

    Remove class attributes

--- interface.py
+++ interface.py
@@ -4,10 +4,6 @@ import tkinter as tk
 class Interface:
     def __init__(self):
         self.root = tk.Tk()
-        self.welcome_lbl = tk.Label(self.root, text="Welcome to Breakout, Game")
-        self.goodbye_lbl = tk.Label(self.root, text="You lost 😓")
-        self.score_lbl = tk.Label(self.root)
-        self.start_btn = tk.Button(self.root, text="Play")
         # Screen setup
         self.root.title("Breakout Game")
         self.root.geometry("800x600")
