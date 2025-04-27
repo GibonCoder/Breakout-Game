e9b07b40f6233f1eb058984775ca8ce128877dbd
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-04-27 21:49:02 +0200

    Change accessibility of brick field

--- brick.py
+++ brick.py
@@ -4,21 +4,21 @@ from random_color_generator import random_color_generator as random_color
 
 class Brick:
     def __init__(self):
-        self.brick = t. Turtle()
-        self.brick.shape('square')
+        self._brick = t. Turtle()
+        self._brick.shape('square')
         t.Screen().colormode(255)
-        self.brick.color(random_color())
-        self.brick.penup()
-        self.brick.speed(0)
-        self.brick.shapesize(stretch_len=1, stretch_wid=1)
+        self._brick.color(random_color())
+        self._brick.penup()
+        self._brick.speed(0)
+        self._brick.shapesize(stretch_len=1, stretch_wid=1)
 
     def get_x(self):
-        return self.brick.xcor()
+        return self._brick.xcor()
 
     def get_y(self):
-        return self.brick.ycor()
+        return self._brick.ycor()
 
     def is_break(self, bricks_list: list):
-        self.brick.hideturtle()
-        self.brick.goto(1000, 1000)
+        self._brick.hideturtle()
+        self._brick.goto(1000, 1000)
         bricks_list.remove(self)
